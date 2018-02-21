7166aa5e7bfbd6cb38be6611c74d0cefe47e9350
karthiss <35914901+karthiss@users.noreply.github.com>
2018-02-21 14:23:11 +0530

    interval odd numbers.c

---
+++ 018.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the Initial Value \n");
+    scanf("%d\n", &a);
+    printf("Enter the Final Value \n");
+    scanf("%d\n", &b);
+    
+    for(c=a+1;c<=b;c++)
+    {
+        if(c%2 !=0)
+        printf(" %d",c);
+    }
+    return 0;
+}
