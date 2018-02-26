d996230ce21f88ae82932f0fc3ceebf4f6131e56
karthiss <35914901+karthiss@users.noreply.github.com>
2018-02-26 14:04:12 +0530

    Composite Number.c

---
+++ 096.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value A  : ");
+    scanf("%d", &a);
+    if(a%2!=0)
+    {
+        if(a%3==0||a%5==0||a%7==0||a%9==0)
+        printf("Yes\n");
+        else
+        printf("No\n");
+    }
+    if(a%2==0)
+    {
+        printf("Invalid Number\n");
+    }
+}
