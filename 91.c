f65710ec68344bb10c6c1c48fbc24579e13b9305
karthiss <35914901+karthiss@users.noreply.github.com>
2018-02-26 13:33:36 +0530

    volume of cuboid.c

---
+++ 91.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d;
+printf("Enter Lenght of the cubiod : ");
+scanf("%d",&a);
+printf("Enter Width of the cubiod : ");
+scanf("%d",&b);
+printf("Enter Height of the cubiod : ");
+scanf("%d",&c);
+d = a*b*c;
+printf("The Volume Of Cubiod is = %d\n", d);
+return 0;
+}
