8e68d7488d06b61dfd276877bba55dae416d6a73
karthiss <35914901+karthiss@users.noreply.github.com>
2018-02-26 13:23:42 +0530

    K is Exists are not.c

---
+++ 112.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter First Value\n");
+	scanf("%d",&a);
+	printf("Enter Second Value\n");
+	scanf("%d",&b);
+	if(b==1||b==2||b==3)
+	
+	printf("YES\n");
+else
+	printf("NO\n");
+return 0;
+}
