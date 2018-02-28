fcd1300027e5481925945be83e89e8d2b86dfeb8
karthiss <35914901+karthiss@users.noreply.github.com>
2018-02-28 14:13:07 +0530

    print Decimal No Using Float.c

---
+++ 082.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter The length of a fram  :");
+    scanf("%f", &a);
+    printf("Enter The breadth of a fram :");
+    scanf("%f", &b);
+    c = a*b;
+    printf("***Area of the fram is : %0.5f ", c);
+}
