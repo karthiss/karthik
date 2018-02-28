f9c6f9873b17c02154fd91deb50ac74c842fcd89
karthiss <35914901+karthiss@users.noreply.github.com>
2018-02-28 14:52:49 +0530

    given string is numeric or not.c

---
+++ 027.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    char ch;
+    printf("Enter A String ");
+    scanf("%d", &a);
+    if (a==ch)
+    printf("No");
+    else
+    printf("Yes");
+    return 0;
+}
