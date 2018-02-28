af2e2aba976a59d5b1e1ba06c15570aeb35fe60d
karthiss <35914901+karthiss@users.noreply.github.com>
2018-02-28 14:34:07 +0530

    arithmetic progression.c

---
+++ 021.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    b = a+c;
+    a = b*3;
+    c = a/2;
+    printf("%d",c);
+}
