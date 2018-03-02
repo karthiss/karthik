bb2e8ee5208318d50bbe897d2a812aaebe704ace
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-02 16:25:42 +0530

    Diff b/w two numbers.c

---
+++ 081.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("Enter the first value :");
+    scanf("%d", &a);
+    printf("Enter the Second Value :");
+    scanf("%d", &b);
+    c = a-b;
+    d = b-a;
+    if (a<b)
+    {
+    printf("%d", d);
+    }
+    else if (b<a)
+    {
+    printf("%d", c);
+    }
+return 0;
+}
