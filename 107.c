814b73166ae456427bf890bbd90fb9a29e901ed8
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-05 14:16:09 +0530

    (a*b)/2.c

---
+++ 107.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the variable A : ");
+    scanf("%d", &a);
+    printf("Enter the Power Value B : ");
+    scanf("%d", &b);
+    printf("Enter the Power Value C : ");
+    scanf("%d", &c);
+    printf("%d",(a*b)/c);
+}
