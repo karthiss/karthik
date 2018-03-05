33ec18aa4e833c4f86c7d3b73f93e0f382effcc6
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-05 13:15:50 +0530

    sum of 4 num.c

---
+++ 092.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,i;
+    printf("Enter the first value :");
+    scanf("%d", &a);
+    printf("Enter the first value :");
+    scanf("%d",&b);
+    printf("Enter the first value :");
+    scanf("%d",&c);
+    printf("Enter the first value :");
+    scanf("%d",&d);
+    i = a+b+c+d;
+    printf("\nThe sum Of Value is %d", i);
+    return 0;
+}
