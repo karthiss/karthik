6d90037c092454b2ec2af93589c5bddca984c473
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-05 13:56:23 +0530

    pow.c

---
+++ 104.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,b,c;
+    printf("Enter the variable A :");
+    scanf("%d", &i);
+    printf("Enter the Power Value B :");
+    scanf("%d", &b);
+    c= pow(i,b) ;
+    printf("The Value is %d \n", c);
+    return 1;
+}
