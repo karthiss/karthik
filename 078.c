89dec0f54e71cc2c32ee87effe6a37abd8f626b3
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-07 14:58:21 +0530

    dived by 13 or not.c

---
+++ 078.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value A  : ");
+    scanf("%d", &a);
+    if(a%13==0)
+    {
+    printf("Yes");
+    }
+    else
+    printf("No");
+    return 0;
+}
