80f3307a730b4d57b038fd96aa7175ae3e785665
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-07 14:16:26 +0530

    arithmetic progression for assume n=c .c

---
+++ 108.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    printf("Enter The First Value  : ");
+    scanf("%d", &a);
+    printf("Enter The Second Value  : ");
+    scanf("%d", &b);
+    printf("Enter The third Value  : ");
+    scanf("%d", &c);
+    d = a+c;
+    e = d*c;
+    f = e/2;
+    printf("%d",f);
+}
