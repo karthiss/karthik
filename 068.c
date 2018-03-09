4b5e49dd4f0cc51fd1f8ad22b48ce4c05d52f56f
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-09 15:50:13 +0530

    multiplication of 7 or not.c

---
+++ 068.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the value : ");
+    scanf("%d", &a);
+    
+    if(a%7==0)
+    {
+        printf("Yes\n");
+    }
+    else
+        printf("No\n");
+    
+}
