377d704bcbcecabad667dd5e1b98be2d439e6eb3
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-16 16:08:06 +0530

    print minimum number for given array.c

---
+++ 023.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+ 
+int main()
+{
+ 
+        int array[50], size, i, min;
+ 
+        printf("\n Enter the size of the array: ");
+	scanf("%d", &size);
+ 
+        printf("\n Enter %d elements of  the array: ", size);
+ 
+        for (i = 0; i < size; i++)
+		scanf("%d", &array[i]);
+ 
+        min = array[0];
+ 
+        for (i = 1; i < size; i++) 
+        {
+		if (min > array[i])
+			min = array[i];
+	}
+ 
+        printf("\n Minimum  element present in the given array is : %d", min);
+ 
+        return 0;
+ 
+}
