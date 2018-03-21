ab4cd5bc0aec49ba6b79a8d604bcdc42f6c6fbd6
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-21 15:55:43 +0530

    swap two numbers.c

---
+++ 037.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the 1st values for swap : ");
+    scanf("%d", &a);
+    printf("Enter the 1st values for swap : ");
+    scanf("%d", &b);
+    
+    printf("%d\n",b );
+    printf("%d",a );
+    return 0;
+}
