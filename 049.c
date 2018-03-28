6036cd9e575f8e720b8023b7608e2e68c8932c7d
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-28 15:56:43 +0530

    sum of two values.c

---
+++ 049.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter The Value : ");
+    scanf("%d", &a);
+    printf("Enter The Value : ");
+    scanf("%d", &b);
+    printf("The Sum Of Two Value Is : %d", a+b);
+    return 0;
+}
