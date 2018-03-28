3682c76706f70483eb77d510090ca9a66e41a56e
karthiss <35914901+karthiss@users.noreply.github.com>
2018-03-28 15:51:59 +0530

    given is range (1,10).c

---
+++ 044.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter The Value : ");
+    scanf("%d", &a);
+    if(1<=a)
+    {
+        if(a<=10)
+        {
+        printf("Yes");
+    }
+    }
+    else
+    printf("No");
+    return 0;
+}
