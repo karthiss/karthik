10edf53381abfa4b7a8d5640d7625f52d6fea184
karthiss <35914901+karthiss@users.noreply.github.com>
2019-08-09 23:49:49 +0530

    Update 013.c

--- 013.c
+++ 013.c
@@ -3,10 +3,10 @@
 int main()
 {
     int i,b,c;
-    printf("Enter the variable A\n");
-    scanf("%d\n", &i);
-    printf("Enter the Power Value B");
-    scanf("%d\n", &b);
+    printf("Enter the variable A : \n");
+    scanf("%d", &i);
+    printf("Enter the Power Value B : ");
+    scanf("%d", &b);
     c= pow(i,b) ;
     printf("The Value is %d \n", c);
     return 1;
