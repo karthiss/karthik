968b0516dd3e5c10ff910d82e7f8ebfcc5586fa0
karthiss <35914901+karthiss@users.noreply.github.com>
2019-08-10 00:30:15 +0530

    Find the entered number is prime or not

--- 066.c
+++ 066.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
     int a;
-    printf("enter the value : ");
+    printf("enter the value A \n");
     scanf("%d", &a);
-    
-    if(a%2!=0)
+    if(a!=1)
     {
-        if(a%3==0||a%5==0||a%7==0||a%9==0)
+    if((a==2)||(a==3)||(a==5)||(a==7)||(a==9))
+    {
+        printf("Yes Prime");
+    }
+    else if(a%2==0||a%3==0||a%5==0||a%7==0||a%9==0)
+       {
         printf("Not prime\n");
-        else
+       }
+       else
+        {
         printf("Yes prime\n");
     }
+    }
+    else
+    {
+        printf("Composite Number");
+    }
+   return 0; 
 }
