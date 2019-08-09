757a39090c40bb5fab2b55c2db79592482c82f9a
karthiss <35914901+karthiss@users.noreply.github.com>
2019-08-09 23:48:06 +0530

    Update 012.c

--- 012.c
+++ 012.c
@@ -3,15 +3,15 @@ void main()
 {
 int a[]={0,1,2,3,4,5,6,7,8,9};
 int sum=0,n,k,i;
- printf("enter n value\n");
-  scanf("%d\n",&n);
-  for(i=0;i<n;i++)
-    printf("%d",a[i]);
-  printf("\nenter K value\n");
-  scanf("%d\n",&k);
-  for(i=0;i<=k;i++)
-  {
-    sum=sum+a[i];
-  }
-  printf("%d\n",sum);
+printf("enter n value 0 to 9\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+printf("%d",a[i]);
+printf("\nenter K value 0 to 9\n");
+scanf("%d",&k);
+for(i=0;i<=k;i++)
+{
+sum=sum+a[i];
+}
+printf("%d\n",sum);
 }
